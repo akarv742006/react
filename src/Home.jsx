4d362da97506e3bca1881c0c95cbbadb5cbcc8c5
akash <akashkrishnamoorthi89@gmail.com>
2026-01-19 19:34:45 +0530

    Replace unreliably unsplash images with static wikimedia images

--- src/Home.jsx
+++ src/Home.jsx
@@ -15,14 +15,14 @@ function Home() {
                         title="Premium College Laptop - Slim, Powerful, All-Day Battery"
                         price={85000}
                         rating={5}
-                        image="https://images.unsplash.com/photo-1517336714731-489689fd1ca8?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+                        image="https://upload.wikimedia.org/wikipedia/commons/e/e9/MacBook_Pro_16-inch_2019.jpg"
                     />
                     <Product
                         id="102"
                         title="Noise Cancelling Wireless Headphones for Study Focus"
                         price={12000}
                         rating={4}
-                        image="https://images.unsplash.com/photo-1546435770-a3e426bf472b?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+                        image="https://upload.wikimedia.org/wikipedia/commons/4/41/Sony_WH-1000XM3.jpg"
                     />
                 </div>
 
@@ -32,21 +32,21 @@ function Home() {
                         title="Spicy Instant Noodles - Pack of 12 (Dorm Essential)"
                         price={450}
                         rating={5}
-                        image="https://images.unsplash.com/photo-1612929633738-8fe44f7ec841?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+                        image="https://upload.wikimedia.org/wikipedia/commons/e/ed/Maggi_Noodles.jpg"
                     />
                     <Product
                         id="104"
                         title="Waterproof Anti-Theft Laptop Backpack with USB Charging Port"
                         price={2500}
                         rating={4}
-                        image="https://images.unsplash.com/photo-1581605405669-fcdf81165afa?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+                        image="https://upload.wikimedia.org/wikipedia/commons/d/d3/Backpack_2020.jpg"
                     />
                     <Product
                         id="105"
                         title="Advanced Engineering Mathematics Textbook"
                         price={850}
                         rating={3}
-                        image="https://images.unsplash.com/photo-1532012197267-da84d127e765?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+                        image="https://upload.wikimedia.org/wikipedia/commons/4/4b/Data_Structures_and_Algorithms_in_Java_6th_Edition.jpg"
                     />
                 </div>
 
@@ -56,7 +56,7 @@ function Home() {
                         title="RGB Mechanical Gaming Keyboard"
                         price={3500}
                         rating={4}
-                        image="https://images.unsplash.com/photo-1595225476474-87563907a212?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+                        image="https://upload.wikimedia.org/wikipedia/commons/6/66/Mechanical_keyboard_RGB.jpg"
                     />
                 </div>
 
@@ -66,28 +66,28 @@ function Home() {
                         title="LED Desk Lamp with Wireless Charger"
                         price={1800}
                         rating={5}
-                        image="https://images.unsplash.com/photo-1534073828943-f801091a7d58?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+                        image="https://upload.wikimedia.org/wikipedia/commons/1/14/Desk_lamp.jpg"
                     />
                     <Product
                         id="108"
                         title="Single Serve Smart Coffee Maker"
                         price={4500}
                         rating={4}
-                        image="https://images.unsplash.com/photo-1517036080480-2b3d358c19a6?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+                        image="https://upload.wikimedia.org/wikipedia/commons/4/45/A_small_cup_of_coffee.JPG"
                     />
                     <Product
                         id="109"
                         title="Classic College Hoodie - Grey"
                         price={1200}
                         rating={4}
-                        image="https://images.unsplash.com/photo-1556905055-8f358a7a47b2?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+                        image="https://upload.wikimedia.org/wikipedia/commons/2/23/Grey_Hoodie.jpg"
                     />
                     <Product
                         id="110"
                         title="5-Subject Notebook Set (Pack of 3)"
                         price={600}
                         rating={5}
-                        image="https://images.unsplash.com/photo-1544816155-12df9643f363?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+                        image="https://upload.wikimedia.org/wikipedia/commons/f/f6/Notebooks_and_Pens.jpg"
                     />
                 </div>
             </div>
