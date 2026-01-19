81080c07e87682b842f4f21ccfe7d6d2449c6981
akash <akashkrishnamoorthi89@gmail.com>
2026-01-19 20:14:04 +0530

    Replace Vaathi Coming with classic hit Munbe Vaa

--- src/MoviePlayer.jsx
+++ src/MoviePlayer.jsx
@@ -19,7 +19,7 @@ function MoviePlayer() {
         's7': { title: 'Kadhale Kadhale - 96', url: 'https://www.youtube.com/embed/lkPI-45gxBw' }, // Verified (Think Music / Lyrics Video - often safer)
         's8': { title: 'Bodhai Kaname - Oh Manapenne', url: 'https://www.youtube.com/embed/mfw2sMi6lrw' },  // Verified
         's9': { title: 'Dandanakka - Romeo Juliet', url: 'https://www.youtube.com/embed/_y-6V1u3mR0' },
-        's10': { title: 'Vaathi Coming - Master', url: 'https://www.youtube.com/embed/vxzfsBDx590' }
+        's10': { title: 'Munbe Vaa - Sillunu Oru Kaadhal', url: 'https://www.youtube.com/embed/1F_U7K1WJvY' }
     };
 
     const song = songData[id] || { title: 'Unknown', url: '' };
